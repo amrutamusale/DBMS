22d6b21f829cc207040c8f46d0bdd157a48e5c6d
amrutamusale <musaleamruta1@gmail.com>
2022-06-02 09:40:56 +0530

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //<data-type>  <variable-name>
+            int a = 5;
+            int b = 5;
+            
+
+            //whether a number is positive or not
+            int positiveNumber = 0;
+            Console.WriteLine();
+            if (positiveNumber > 0)
+            {
+                Console.WriteLine(positiveNumber + " is a positive Number");
+            }
+            else if (positiveNumber < 0)
+            {
+                Console.WriteLine(positiveNumber + " is not a positive Number");
+            }
+            else
+            {
+                Console.WriteLine(positiveNumber + " is neither postive nor negative");
+            }
+        }
+    }
+}
